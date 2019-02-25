b6a309bf87b9b127ffe472905b720d3216c0ae9c
danolivier <dan.olivier0912@gmail.com>
2019-02-25 12:34:57 +0100

    day correction + remove padding on shows

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -16,12 +16,13 @@ class Grid extends Component {
     return this.padStr(dte.getDate()) + '/' + this.padStr(dte.getMonth() + 1) + '/' + dte.getFullYear();
   }
 
-  secondsToHours(val) {
-    return val / 3600;
+  secondsToHours(value) {
+    return value/3600;
   }
 
   render() {
     const { data } = this.props;
+    const dayHeight = this.secondsToHours(data.endTime - data.startTime) * 300;
 
     return (
       <div className="Grid">
@@ -32,16 +33,18 @@ class Grid extends Component {
           {data.chns.map(chn =>
             <div key={chn.key} className="Chn">
               <div className="Chn-title">{chn.key}</div>
-              {chn.shows.map((show, i) => {
-                  const showLength = this.secondsToHours(show.endTime - show.startTime);
-                  const showHeight = showLength * 150;
+              <div className="shows" style={{height: dayHeight}}>
+                {chn.shows.map((show, i) => {
+                  const showLength = this.secondsToHours(show.endTime - show.startTime) * 300;
+
                   return (
-                      <div key={i} style={{minHeight: showHeight + 'px'}} className="show">
+                      <div key={i} style={{height: showLength + 'px'}} className="show">
                         <p className="show-title">{show.title}</p>
                         {show.subTitle ? <p>{show.subTitle}</p> : ''}
                       </div>
                   )
-              })}
+                })}
+              </div>
             </div>
            )}
         </div>
